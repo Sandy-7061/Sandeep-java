230ee2a066f9ffe654da08b5dac610f019f6c1dd
Sandeep <sandeepkush880@gmail.com>
2026-03-26 16:22:26 +0530

    Casting

---
+++ Core Java/casting/casst.java
@@ -0,0 +1,26 @@
+package casting;
+
+class Casst{
+
+
+    public static void main(String[] args) {
+
+        // Question = Create a double variable with the value 99.99. Cast it to an int and print both the original and the casted value.
+        // double var = 99.99;
+        // System.err.println("The Orignal value is "+ var +" The Casted value is " + (int)var);
+
+        //Create an int variable with a value of 67. Cast this integer to a char and print it. Then, take the character 'Z' and cast it to an int to find its Unicode value.
+
+        // int var2 = 67;
+        // System.out.println(" The value is " + (char)var2 );
+
+        // char a = 'Z';
+        //  System.err.println((int)a);
+
+        //3
+        //Create an int a = 5; and a double b = 2.0;. Calculate a / b and store it in a variable. Print the result. Then, try to store a / b into an int variable using a cast.
+        // int a = 5;
+        // double b = 2.0;
+        //  System.err.println("Answer of a/b is = " + a/(int)b);
+    }
+}
